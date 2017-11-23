f21f3a6e801ab44419bc21db415b2b676714cdb0
Cihan Tas <cihantas@users.noreply.github.com>
2017-11-23 15:26:28 +0100

    User Follows endpoint.

---
+++ twitch/users_follows.go
@@ -0,0 +1,28 @@
+package twitch
+
+import "net/url"
+
+// UsersGetCall represents a GET request to the /users endpoint.
+type UsersFollowsGetRequest struct {
+	service   *Service
+	urlParams url.Values
+}
+
+// UserFollow represents a follow relationship between to Twitch users.
+type UserFollow struct {
+	FollowedAt string `json:"followed_at,omitempty"`
+	FromID     string `json:"from_id,omitempty"`
+	ToID       string `json:"to_id, omitempty"`
+}
+
+// UsersFollowsGetResponse represents a response returned by the endpoint /users/follows.
+type UsersFollowsGetResponse struct {
+	Data       []UserFollow `json:"data"`
+	Pagination Pagination
+}
+
+// Get returns an instance of UsersFollowsGetCall.
+func (s *UsersService) GetFollows() *UsersFollowsGetRequest {
+	c := &UsersFollowsGetRequest{service: s.service, urlParams: make(map[string][]string)}
+	return c
+}
